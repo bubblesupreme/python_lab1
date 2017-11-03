68ab61bfc8e670ade5a08fbb7f35dfd6da1bf918
bubblesupreme <32437473+bubblesupreme@users.noreply.github.com>
2017-11-03 23:06:54 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,120 +1,205 @@
 f=open("birthday.txt","r")
+# общая "база"-список из списка с именем и датой рождения
 baza=[]
-for line in f.readlines():
-    line=line.split(':')
-    baza.append(line)
-f.close()
-print(baza)
-print("Enter add, delete, change, age, next birthday, birthday or exit")
-aa=input()
-while aa!="exit":
-    if aa=="add":
-        print("Enter name and day of birthday\n(name : dd.mm.yyyy)")
-        line=input()
-        line += '\n'
-        line = line.split(':')
+
+# функция, работающая с чтением файла
+# заносит данные с файла в базу
+def read_file():
+    for line in f.readlines():
+        if (line[-1] == '\n'):
+            line = line[:-1]
+        line = line.split(' : ')
         baza.append(line)
-        print(line)
-    if aa=="delete":
-        print("Enter name")
-        name=input()
-        name+=' '
-        k=False
-        for item in baza:
-            if item[0]==name:
-                baza.remove(item)
-                print("Successfully deleted ",item[0],item[1])
-                k=True
-                break
-        if (k==False):
-            print("There is no ",name,"in database!")
-    if aa=="change":
+    f.close()
+
+# функция, добавляющая новые данные
+def add():
+    print("Enter name and day of birthday\n(name : dd.mm.yyyy)")
+    line = input()
+    # проверка на правильность ввода данных
+    if ' : ' in line:
+        line = line.split(' : ')
+        # проверка на правильность ввода данных о дне рождении
+        if len(line[1])==10 and int(line[1][0:2])<=31 and int(line[1][3:5])<=12 and line[1][2]=='.' and line[1][5]=='.':
+            # если человек с таким именем уже занесен в базу
+            for item in baza:
+                if line[0]==item[0]:
+                    return "Error! Already added!"
+            baza.append(line)
+            return "Successfully added "+line[0]+' : '+line[1]
+        else:
+            print("Error! Wrong format of birthday, try again")
+            add()
+    else:
+        print("Error! Wrong format string, try again")
+        add()
+
+# функция, удаляющая данные
+def delete():
+    # проверка на пустоту базы
+    if (len(baza) == 0):
+        return "Database is empty!"
+    else:
         print("Enter name")
         name = input()
-        name += ' '
-        k = False
         for item in baza:
             if item[0] == name:
-                print("Change name or birthday?")
-                temp=input()
-                if(temp=="name"):
-                    print("Enter new name")
-                    name = input()
-                    name += ' '
-                    item[0]=name
-                if (temp == "birthday"):
-                    print("Enter new birthday\n(dd.mm.yyy)")
-                    birthday= input()
-                    birthday=' '+birthday+'\n'
-                    item[1] = birthday
-                print("Successfully changed to", item[0], item[1])
-                k = True
-                break
-        if (k==False):
-            print("There is no ",name,"in database!")
-    if aa=="age":
+                baza.remove(item)
+                return "Successfully deleted "+ item[0]+' : '+item[1]
+        # если человека с таким именем нет
+        return "There is no "+name+ " in database!"
+
+# функция, изменяющая данные
+def change():
+    # проверка на пустоту базы
+    if (len(baza) == 0):
+        return "Database is empty!"
+    else:
         print("Enter name")
         name = input()
-        name += ' '
-        k=False
+        # ищем в базе данные с таким именем
+        for item in baza:
+            if item[0] == name:
+                # если нашли
+                test=False
+                while(test==False):
+                    print("Change name or birthday?")
+                    temp = input()
+                    if (temp == "name"):
+                        print("Enter new name")
+                        name = input()
+                        item[0] = name
+                        test=True
+                    if (temp == "birthday"):
+                        test_b=False
+                        print("Enter new birthday\n(dd.mm.yyy)")
+                        while test_b==False:
+                            birthday = input()
+                            # проверяем на правильнность ввода данных о дне рождении
+                            if len(birthday) == 10 and int(birthday[0:2]) < 31 and int(birthday[3:5]) < 12 and birthday[2] == '.' and birthday[5] == '.':
+                                item[1] = birthday
+                                test_b=True
+                                test=True
+                            else:
+                                print("Error! Wrong format of birthday, try again")
+                                print("Enter new birthday\n(dd.mm.yyy)")
+                    if (test==False):
+                        print("Error! Wrong command!")
+                return "Successfully changed to "+item[0]+' : '+item[1]
+        return "There is no "+name+ " in database!"
+
+# функция, вычисляющая возраст
+def age():
+    # проверка на пустоту базы
+    if (len(baza) == 0):
+        return "Database is empty!"
+    else:
+        print("Enter name")
+        name = input()
         for item in baza:
             if item[0] == name:
                 from datetime import timedelta, datetime
+                # вычисляем нынешнюю дату
                 now = datetime.now()
-                temp=item[1]
-                birthday = datetime(int(temp[7:11]),int(temp[4:6]),int(temp[1:3]))
-                period = now-birthday
-                print (item[0]," is ",period.days//365," years old")
-                k = True
-                break
-        if (k == False):
-            print("There is no ", name, "in database!")
-    if aa == "next birthday":
-        if (len(baza)==0):
-            print("Database is empty!")
-        else:
-             from datetime import timedelta, datetime
-             now = datetime.now()
-             min=365
-             for item in baza:
                 temp = item[1]
-                next=[]
-                birthday = datetime(int(now.strftime("%Y")), int(temp[4:6]), int(temp[1:3]))
-                period = birthday - now
-                day=period.days
-                if day<0:
-                    day = 365+day
-                if (day)<min:
-                    min=day
-                    next=item
-             print(next[0],':',next[1])
-    if aa=="birthday":
+                # переводит строку с датой рождения в формат даты
+                birthday = datetime(int(temp[6:10]), int(temp[3:5]), int(temp[0:2]))
+                # сравниваем даты, находим разницу в днях
+                period = now - birthday
+                # вычисляем кол-во целых лет
+                return item[0]+" is "+str(period.days // 365)+" years old"
+        return "There is no "+ name+ " in database!"
+
+# функция, вычисляющая ближайший день рождения
+def next_birthday():
+    # проверка на пустоту базы
+    if (len(baza) == 0):
+        return "Database is empty!"
+    else:
+        from datetime import timedelta, datetime
+        # вычисляем нынешнюю дату
+        now = datetime.now()
+        # минимальная разница
+        min = 365
+        next = []
+        for item in baza:
+            temp = item[1]
+            # переводит строку с датой рождения в формат даты
+            # при этом год заменяем на текущий
+            birthday = datetime(int(now.strftime("%Y")), int(temp[3:5]), int(temp[0:2]))
+            # сравниваем разницу
+            period = birthday - now
+            day = period.days
+            # если разница меньше -1 ->день рождение в этом году уже прошло -> прибавляем 365
+            if day < -1:
+                day = 365 + day
+            # сравниваем с минимумом
+            if day < min:
+                min = day
+                next.clear()
+                next.append(item)
+            else:
+                if day == min:
+                    next.append(item)
+        str=""
+        for item in next:
+            str+= item[0]+' : '+item[1]+', '
+        return str
+
+# функция, находящая людей с днем рождения в определенный день, месяц или год
+def birthday():
+    # проверка на пустоту базы
+    if (len(baza) == 0):
+        return "Database is empty!"
+    else:
         print("Enter date\n(dd.mm, mm or yyyy)")
-        date=input()
-        k=False
-        if len(date)==5:
+        date = input()
+        str=''
+        if len(date) == 5:
             for item in baza:
-                birthday=item[1]
-                if (birthday[1:6]==date):
-                    print(item[0])
-                    k=True
-        if len(date)==2:
+                birthday = item[1]
+                if (birthday[0:5] == date):
+                    str+=item[0]+', '
+        if len(date) == 2:
             for item in baza:
-                birthday=item[1]
-                if (birthday[4:6]==date):
-                    print(item[0])
-                    k = True
-        if len(date)==4:
+                birthday = item[1]
+                if (birthday[3:5] == date):
+                    str+=item[0]+', '
+        if len(date) == 4:
             for item in baza:
-                birthday=item[1]
-                if (birthday[7:11]==date):
-                    print(item[0])
-                    k = True
-        if k==False:
-            print("There is no ", date, "in database!")
+                birthday = item[1]
+                if (birthday[6:10] == date):
+                    str+=item[0]+', '
+        if str != '':
+            return str
+        else:
+            return "There is no "+ date+ " date in database!"
+
+#функция, работающая с записью в файл
+#заносит данные в файл из базы
+def write_file():
+    f = open("birthday.txt", "w")
+    for item in baza:
+        f.write(" : ".join(item) + "\n")
+    f.close()
+
+def main():
+    functions={'add':add,
+              'delete':delete,
+              'change':change,
+              'age':age,
+              'next birthday':next_birthday,
+              'birthday':birthday}
+    read_file()
     print("Enter add, delete, change, age, next birthday, birthday or exit")
-    aa = input()
-f=open("birthday.txt","w")
-for item in baza:
-    f.write(":".join(item))
-f.close()
+    aa=input()
+    while aa!="exit":
+        if aa in functions.keys():
+            print(functions[aa]())
+        else:
+            print("Error! Wrong command!")
+        print("Enter add, delete, change, age, next birthday, birthday or exit")
+        aa = input()
+    write_file()
+
+main()
--- task2.py
+++ task2.py
@@ -1,58 +1,85 @@
-f=open("collection.txt","r")
+#список, хранящий списки, состоящие из имени и списка коллекции имеющихся момент
 collection=[]
-for line in f.readlines():
-    line=line[0:-1]
-    line=line.split(' : ')
-    line[1]=line[1].split(', ')
-    collection.append(line)
-f.close()
-f=open("wanted.txt","r")
+#список, хранящий списки из имени и 1 желаемой монеты
 wanted=[]
-for line in f.readlines():
-    line=line.split(' : ')
-    line[1]=line[1][0:-1]
-    wanted.append(line)
-f.close()
-print(collection)
+#множество всех имен
 all_names=set()
-for item in collection:
-    all_names.add(item[0])
-print("Enter the participants or exit:")
-aa=input()
-while(aa!="exit"):
-    participants=set(aa.split(' '))
 
-    print(participants)
-    temp=participants.difference(all_names)
-    if len(temp)!=0:
-        print("There is no ",temp)
+#функция для работы с файлом с коллекцией монет
+def open_coll():
+  f=open("collection.txt","r")
+  for line in f.readlines():
+      line=line[0:-1]
+      line=line.split(' : ')
+      line[1]=line[1].split(', ')
+      collection.append(line)
+  f.close()
+  for item in collection:
+      all_names.add(item[0])
+
+#функция для работы с файлом с монетами, которые коллекционер хочет получить
+def open_want():
+  f=open("wanted.txt","r")
+  for line in f.readlines():
+      line=line.split(' : ')
+      line[1]=line[1][0:-1]
+      wanted.append(line)
+  f.close()
+
+#основная функция, определяющая возможность обмена, возвращает строку
+def check(participants):
+  #проверяем все ли введенные имена содержатся во множестве всех имен
+  temp = participants.difference(all_names)
+  #если есть имена, не принадлежащие множуству всех имен, возвращаем соответствующую строку
+  if len(temp) != 0:
+    str="There is no: "
+    for item in temp:
+      str+=item+', '
+    return str
+  #если таких элементов не нашлось
+  else:
+    #множество всех монет, которые хотят получить участники обмена
+    want_s = set()
+    #множество всех монет участников, доступных для обмена
+    collect_s = set()
+    #составляем мн-во collect_s
+    for item in collection:
+      temp = set()
+      temp.add(item[0])
+      if temp.issubset(participants) == True:
+        for items in item[1]:
+          collect_s.add(items)
+    #составляем мн-во want_s
+    for item in wanted:
+      temp = set()
+      temp.add(item[0])
+      if temp.issubset(participants) == True:
+        want_s.add(item[1])
+    #проверяем все ли элементы want_s входят в collect_s
+    if want_s.issubset(collect_s):
+      #если да, продолжаем проверку
+      #проверяем имеет ли каждый участник обмена хотя бы 1 монету,
+      # которую желает получить другой участник,
+      # т.е. как минимум 1 монета должна входить в want_s
+      for item in collection:
+        if set(item[0]).issubset(participants):
+          if set(item[1]).isdisjoint(want_s):
+            #если у участника ни одна монета не принадлежит want_s
+            #обмен не возможен
+            return "The exchange is impossible!"
+      return "The exchange is possible."
     else:
-        want_s=set()
-        collect_s=set()
-        test=False
-        for item in collection:
-            temp=set()
-            temp.add(item[0])
-            if temp.issubset(participants)==True:
-                for items in item[1]:
-                    collect_s.add(items)
-    #print("collect",collect_s)
-        for item in wanted:
-            temp = set()
-            temp.add(item[0])
-            if temp.issubset(participants) == True:
-                want_s.add(item[1])
-    #print(want_s)
-        if want_s.issubset(collect_s):
-            for item in collection:
-                if set(item[0]).issubset(participants):
-                    if set(item[1]).isdisjoint(want_s):
-                        test=True
-                        print("The exchange is impossible!")
-                        break
-            if test==False:
-                print("The exchange is possible.")
-        else:
-            print("The exchange is impossible!")
-    print("Enter the participants or exit:")
-    aa = input()
+      return "The exchange is impossible!"
+
+def main():
+  open_coll()
+  open_want()
+  print("Enter the participants or exit:")
+  aa=input()
+  while(aa!="exit"):
+      participants=set(aa.split(' '))
+      print(check(participants))
+      print("Enter the participants or exit:")
+      aa = input()
+
+main()
